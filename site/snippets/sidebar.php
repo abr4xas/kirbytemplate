8174b10ce1e79514b65c1fe8566fd5fd454a6bd5
abr4xas <me@abr4xas.org>
2014-02-08 08:41:31 -0430

    some changes

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -66,53 +66,56 @@
         <h4 class="modal-title">Aviso legal</h4>
       </div>
       <div class="modal-body">
-   <p>El sitio web abr4xas.org comunica a los usuarios, a través de este aviso, que puede utilizar cookies cuando el usuario navega por las diferentes pantallas y páginas del sitio.</p>
-<p>Durante el uso de nuestro sitio web usted acepta y autoriza expresamente el uso de cookies, de acuerdo con nuestra Política de Privacidad.</p>
-<h2>¿Qué son las cookies?</h2>
-<p>Una cookie es un archivo de texto muy pequeño que un servidor Web puede guardar en el disco duro de un equipo para almacenar algún tipo de informaci&oacute;n sobre el usuario. La cookie identifica el equipo de forma única, y s&oacute;lo puede ser leída por el sitio Web que lo envi&oacute; al equipo.</p>
+   <p>El sitio web abr4xas.org comunica a los usuarios, a trav&eacute;s de este aviso, que puede utilizar cookies cuando el usuario navega por las diferentes pantallas y p&aacute;ginas del sitio.</p>
+<p>Durante el uso de nuestro sitio web usted acepta y autoriza expresamente el uso de cookies, de acuerdo con nuestra Pol&iacute;tica de Privacidad.</p>
+<h2>&iquest;Qu&eacute; son las cookies?</h2>
+<p>Una cookie es un archivo de texto muy peque&ntilde;o que un servidor Web puede guardar en el disco duro de un equipo para almacenar alg&uacute;n tipo de informaci&oacute;n sobre el usuario. La cookie identifica el equipo de forma &uacute;nica, y s&oacute;lo puede ser le&iacute;da por el sitio Web que lo envi&oacute; al equipo.</p>
 <p>Una cookie no es un archivo ejecutable ni un programa y por lo tanto no puede propagar o contener un virus u otro software malicioso, ni puede tener una longitud superior a 4.000 caracteres.</p>
-<h2>¿Para qué sirven las cookies?</h2>
-<p>La utilizaci&oacute;n de las cookies tiene como finalidad exclusiva recordar las preferencias del usuario (idioma, país, inicio de sesi&oacute;n, características de su navegador, informaci&oacute;n de uso de nuestra Web, etc.)</p>
-<p>Recordando sus preferencias no tendremos que, por ejemplo, preguntarle por su país cada vez que visite nuestra Web. Sabremos las características del ordenador que está usando y así podremos ofrecerle una mejor experiencia de navegaci&oacute;n. Las cookies pueden ayudar a nuestro sitio Web a distinguir el navegador del usuario como visitante anterior y así guardar y recordar las preferencias que puedan haberse establecido mientras el usuario estaba navegando por el sitio, personalizar las páginas de inicio, identificar qué sectores de un sitio han sido visitados o mantener un registro de selecciones en un “carro de compra”.</p>
-<p>El sitio web abr4xas.org puede utilizar las cookies con el objeto de reconocer a los usuarios que se hayan registrado y poder ofrecerles un mejor servicio y más personalizado. Asimismo pueden ser utilizadas para obtener informaci&oacute;n acerca de la fecha y hora de la última visita del usuario, medir algunos parámetros de tráfico dentro del propio site y estimar el número de visitas realizadas, de manera que abr4xas.org pueda enfocar y ajustar los servicios y promociones de forma más efectiva.</p>
-<p>Normalmente los sitios Web utilizan las cookies para obtener informaci&oacute;n estadística sobre sus páginas Web. Tenga en cuenta que recogemos datos sobre sus movimientos y uso de nuestra web como datos estadísticos, no personales.</p>
+<h2>&iquest;Para qu&eacute; sirven las cookies?</h2>
+<p>La utilizaci&oacute;n de las cookies tiene como finalidad exclusiva recordar las preferencias del usuario (idioma, pa&iacute;s, inicio de sesi&oacute;n, caracter&iacute;sticas de su navegador, informaci&oacute;n de uso de nuestra Web, etc.)</p>
+<p>Recordando sus preferencias no tendremos que, por ejemplo, preguntarle por su pa&iacute;s cada vez que visite nuestra Web. Sabremos las caracter&iacute;sticas del ordenador que est&aacute; usando y as&iacute; podremos ofrecerle una mejor experiencia de navegaci&oacute;n. 
+Las cookies pueden ayudar a nuestro sitio Web a distinguir el navegador del usuario como visitante anterior y as&iacute; guardar y recordar las preferencias que puedan haberse establecido mientras el usuario estaba navegando por el sitio, personalizar las p&aacute;ginas de inicio, 
+identificar qu&eacute; sectores de un sitio han sido visitados o mantener un registro de selecciones en un "carro de compra".</p>
+<p>El sitio web abr4xas.org puede utilizar las cookies con el objeto de reconocer a los usuarios que se hayan registrado y poder ofrecerles un mejor servicio y m&aacute;s personalizado. Asimismo pueden ser utilizadas para obtener informaci&oacute;n acerca de la fecha y hora de la &uacute;ltima visita del usuario, 
+medir algunos par&aacute;metros de tr&aacute;fico dentro del propio site y estimar el n&uacute;mero de visitas realizadas, de manera que abr4xas.org pueda enfocar y ajustar los servicios y promociones de forma m&aacute;s efectiva.</p>
+<p>Normalmente los sitios Web utilizan las cookies para obtener informaci&oacute;n estad&iacute;stica sobre sus p&aacute;ginas Web. Tenga en cuenta que recogemos datos sobre sus movimientos y uso de nuestra web como datos estad&iacute;sticos, no personales.</p>
 <h2>Tipos de cookies</h2>
-<p>Cookies analíticas: Recogen informaci&oacute;n del uso que se realiza del sitio web.</p>
+<p>Cookies anal&iacute;ticas: Recogen informaci&oacute;n del uso que se realiza del sitio web.</p>
 <p>Cookies sociales: Son necesarias para las redes sociales externas (Facebook, Twitter Linkedin).</p>
-<p>Cookies técnicas: Son las estrictamente necesarias para el uso del sitio Web y para la prestaci&oacute;n del servicio contratado.</p>
+<p>Cookies t&eacute;cnicas: Son las estrictamente necesarias para el uso del sitio Web y para la prestaci&oacute;n del servicio contratado.</p>
 <h2>Tipos y finalidades de las cookies.</h2>
-<p>A continuaci&oacute;n se presenta una lista de las cookies que puede encontrar en nuestro sitio Web y una breve descripci&oacute;n de su finalidad. Le aseguramos que ninguna de ellas conserva informaci&oacute;n de identificaci&oacute;n personal sobre usted. Algunas cookies personalizan su visita, otras recuerdan sus preferencias, mientras que otras solo nos proveen estadísticas de los visitantes.</p>  
+<p>A continuaci&oacute;n se presenta una lista de las cookies que puede encontrar en nuestro sitio Web y una breve descripci&oacute;n de su finalidad. Le aseguramos que ninguna de ellas conserva informaci&oacute;n de identificaci&oacute;n personal sobre usted. Algunas cookies personalizan su visita, otras recuerdan sus preferencias, mientras que otras solo nos proveen estad&iacute;sticas de los visitantes.</p>  
                   
 <dl>
   <dt>__utma</dt>
-  <dd>Identificaci&oacute;n de visitantes únicos: Utilizada para registrar visitantes únicos con fines estadísticos y analíticos. Duraci&oacute;n: 2 años y pertenece a Google (Analytic).</dd>
+  <dd>Identificaci&oacute;n de visitantes &uacute;nicos: Utilizada para registrar visitantes &uacute;nicos con fines estad&iacute;sticos y anal&iacute;ticos. Duraci&oacute;n: 2 a&ntilde;os y pertenece a Google (Analytic).</dd>
   <dt>__utmb</dt>
   <dd>Determinaci&oacute;n de la sesi&oacute;n del visitante: Google Analytics utiliza dos cookies para establecer una sesi&oacute;n. Si falta alguna de estas dos cookies, otra actividad inicia el comienzo de una nueva sesi&oacute;n. Duraci&oacute;n: 30 minutos y pertenece a Google (Analytic).</dd>   
   <dt>__utmc</dt>
   <dd>Determinaci&oacute;n de la sesi&oacute;n del visitante: Google Analytics utiliza dos cookies para establecer una sesi&oacute;n. Si falta alguna de estas dos cookies, otra actividad inicia el comienzo de una nueva sesi&oacute;n.. Si falta alguna de estas dos cookies, otra actividad inicia el comienzo de una nueva sesi&oacute;n. Es una cookie de sesi&oacute;n y pertenece a Google (Analytic).</dd>        
   <dt>__utmz</dt>
-  <dd>Registro de fuentes de tráfico y navegaci&oacute;n: Utilizado para registrar c&oacute;mo lleg&oacute; a nuestro sitio Web (por una búsqueda de Google, un aviso, etc.) y los recorridos que sigue mientras navega nuestro sitio. Utilizamos esta informaci&oacute;n para mejorar la experiencia del usuario en futuras actualizaciones. Duraci&oacute;n: 6 meses y pertenece a Google (Analytic).</dd>      
+  <dd>Registro de fuentes de tr&aacute;fico y navegaci&oacute;n: Utilizado para registrar c&oacute;mo lleg&oacute; a nuestro sitio Web (por una b&uacute;squeda de Google, un aviso, etc.) y los recorridos que sigue mientras navega nuestro sitio. Utilizamos esta informaci&oacute;n para mejorar la experiencia del usuario en futuras actualizaciones. Duraci&oacute;n: 6 meses y pertenece a Google (Analytic).</dd>      
 </dl>
 
 <h2>Salvaguardias de protecci&oacute;n<br>
 </h2>
 <p>El usuario puede configurar su navegador para aceptar, o no, las cookies que recibe o para que el navegador le avise cuando un servidor quiera guardar una cookie o borrarlas de su ordenador. Puede encontrar las instrucciones en la configuraci&oacute;n de seguridad en su navegador Web.</p>
 <p>Ejemplos:</p>
-<p>• Si utiliza Microsoft Internet Explorer, en la opci&oacute;n de menú Herramientas &gt; Opciones de Internet &gt; Privacidad &gt; Configuraci&oacute;n.</p>
-<p>• Si utiliza Firefox, en la opci&oacute;n de menú Herramientas &gt; Opciones &gt; Privacidad &gt; Cookies.</p>
-<p>• Si utiliza Google Chrome, en la opci&oacute;n de menú Configuraci&oacute;n &gt; Privacidad</p>
-<p>• Si utiliza Safari, en la opci&oacute;n de menú Preferencias &gt; Seguridad</p>
-<p>Puede hacer uso de la secci&oacute;n “Ayuda” que encontrará en la barra de herramientas de la mayoría de navegadores para cambiar los ajustes de su ordenador, sin embargo, algunas de las características de nuestros servicios online pueden no funcionar o pueden resultar más complicadas de acceder si rechaza todas las cookies.</p>
-<p>Muchos navegadores permiten activar un modo privado mediante el cual las cookies se borran siempre después de su visita. Dependiendo de cada navegador, este modo privado puede tener diferentes nombres, abajo puede encontrar una lista de los navegadores más comunes y los diferentes nombres de este “modo privado”:</p>
+<p> Si utiliza Microsoft Internet Explorer, en la opci&oacute;n de men&uacute; Herramientas &gt; Opciones de Internet &gt; Privacidad &gt; Configuraci&oacute;n.</p>
+<p> Si utiliza Firefox, en la opci&oacute;n de men&uacute; Herramientas &gt; Opciones &gt; Privacidad &gt; Cookies.</p>
+<p> Si utiliza Google Chrome, en la opci&oacute;n de men&uacute; Configuraci&oacute;n &gt; Privacidad</p>
+<p> Si utiliza Safari, en la opci&oacute;n de men&uacute; Preferencias &gt; Seguridad</p>
+<p>Puede hacer uso de la secci&oacute;n "Ayuda" que encontrar&aacute; en la barra de herramientas de la mayor&iacute;a de navegadores para cambiar los ajustes de su ordenador, sin embargo, algunas de las caracter&iacute;sticas de nuestros servicios online pueden no funcionar o pueden resultar m&aacute;s complicadas de acceder si rechaza todas las cookies.</p>
+<p>Muchos navegadores permiten activar un modo privado mediante el cual las cookies se borran siempre despu&eacute;s de su visita. Dependiendo de cada navegador, este modo privado puede tener diferentes nombres, abajo puede encontrar una lista de los navegadores m&aacute;s comunes y los diferentes nombres de este "modo privado":</p>
 <p>- Internet Explorer 8 y superior: InPrivate</p>
 <p>- FireFox 3.5 y superior: Navegaci&oacute;n Privada</p>
 <p>- Google Chrome 10 y superior: Inc&oacute;gnito</p>
 <p>- Safari 2 y superior: Navegaci&oacute;n Privada</p>
 <p>- Opera 10.5 y superior: Navegaci&oacute;n Privada</p>
-<p>Por favor, lea atentamente la secci&oacute;n de ayuda de su navegador para conocer más acerca de como activar el “modo privado”. Podrá seguir visitando nuestra Web aunque su navegador esté en “modo privado”, no obstante, la experiencia de usuario puede no ser &oacute;ptima y algunas utilidades pueden no funcionar.</p>
-<p>abr4xas.org le agradece que active la aceptaci&oacute;n de cookies, esto nos ayuda a obtener datos más precisos que nos permiten mejorar el contenido y el diseño de nuestra página Web para adaptarlo a sus preferencias.</p>
-<h2>Cambios en la Política de Cookies</h2>
-<p>Es posible que actualicemos la Política de Cookies de nuestro Sitio Web, por ello le recomendamos revisar esta política cada vez que acceda a nuestro Sitio Web con el objetivo de estar adecuadamente informado sobre c&oacute;mo y para qué usamos las cookies.</p> 
+<p>Por favor, lea atentamente la secci&oacute;n de ayuda de su navegador para conocer m&aacute;s acerca de como activar el "modo privado". Podr&aacute; seguir visitando nuestra Web aunque su navegador est&eacute; en "modo privado", no obstante, la experiencia de usuario puede no ser &oacute;ptima y algunas utilidades pueden no funcionar.</p>
+<p>abr4xas.org le agradece que active la aceptaci&oacute;n de cookies, esto nos ayuda a obtener datos m&aacute;s precisos que nos permiten mejorar el contenido y el dise&ntilde;o de nuestra p&aacute;gina Web para adaptarlo a sus preferencias.</p>
+<h2>Cambios en la Pol&iacute;tica de Cookies</h2>
+<p>Es posible que actualicemos la Pol&iacute;tica de Cookies de nuestro Sitio Web, por ello le recomendamos revisar esta pol&iacute;tica cada vez que acceda a nuestro Sitio Web con el objetivo de estar adecuadamente informado sobre c&oacute;mo y para qu&eacute; usamos las cookies.</p> 
       </div>
       <div class="modal-footer">
           <button type="button" class="btn btn-primary btn-lg btn-block" data-dismiss="modal"><span class="glyphicon glyphicon-ok"></span>
